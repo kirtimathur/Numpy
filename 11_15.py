5d0f6a3dede5fe48c219e2326415d9cca35e8fb2
Kirti Mathur <kirtimathur67@gmail.com>
2022-11-15 21:43:18 +0530

    Add files via upload

---
+++ 11_15.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#q. 1 How to find the memory size of any array
+import numpy as np
+a=np.array([1,2,3])
+print("size of array:",a.itemsize*a.size)
+
+
+# In[2]:
+
+
+#q.2 create an array of (3,5,8 )
+a1=np.ones((3,5,8))
+print(a1)
+
+
+# In[ ]:
+
+
+
+
